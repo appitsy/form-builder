5485438ee7c4cf77a5b7e9a56653949bf434867e
Paras Gera <paras@appitsy.com>
2020-12-23 21:55:50 +0530

    html and styling changes to make it web ready

--- src/App.tsx
+++ src/App.tsx
@@ -15,65 +15,82 @@ import styled from '@emotion/styled';
 import Designer from './Components/Designer';
 import { ComponentSchemaWithId } from './Components/DesignerRenderer';
 
-const AppTabs = styled(Tabs)`
-    display: flex;
-    flex-direction: column;
-    height: 100%;
-
-    .appitsy-builder-header-tabs {
-        margin-bottom: 0px;
-        border-bottom: 4px darkgrey solid;
-        padding: 0px 200px;
-    
-        .react-tabs__tab {
-            width: 33.33%;
-            text-align: center;
-            background: WhiteSmoke;
-        }
-    
-        .react-tabs__tab--selected {
-            background: lightgray;
-        }
-    
-        .react-tabs__tab-list {
-            border-width: 0px;
-        }
-    
-        .react-tabs__tab {
-            border-width: 0px 1px 0px 1px;
-            border-color: #909090;
-            border-radius: 0px;
-            bottom: 0px;
-        }
-    }
+const AppRoot = styled.div`
+	display: flex;
+	flex-direction: column;
+	flex-grow: 1;
 `;
 
+const Heading = styled.h4`
+	margin: 10px 0px;
+	text-align: center;
+`
+const AppTabs = styled(Tabs)`
+	display: flex;
+  flex-direction: column;
+	flex-grow: 1;
+
+  .appitsy-builder-header-tabs {
+    margin: 15px auto;
+
+    .react-tabs__tab-list {
+      border-width: 0px;
+    }
+
+    .react-tabs__tab {
+      width: 150px;
+      border: 0px;
+      bottom: 0px;
+      border-radius: 20px;
+      margin: 0px 10px;
+      text-align: center;
+      background: WhiteSmoke;
+      box-shadow: none;
+    }
+
+    .react-tabs__tab--selected {
+      background: #e6e6e6;
+      box-shadow: 0 0 5px #686d75;
+    }
+  }
+`;
+
+const tabPanelStyle = { 
+	flexGrow: 1,
+	borderTop: '2px solid #cfcfcf',
+};
+
 const App = () => {
-    const [data, setData] = useState({});
-    const [schema, setSchema] = useState<ComponentSchemaWithId[]>([]);
+  const [data, setData] = useState({});
+  const [schema, setSchema] = useState<ComponentSchemaWithId[]>([]);
 
-    return (
-        <AppTabs>
+  return (
+    <AppRoot>
+      <Heading>Form Builder</Heading>
+      <AppTabs>
+        <TabList className="appitsy-builder-header-tabs">
+          <Tab>Designer</Tab>
+          <Tab>Schema JSON</Tab>
+          <Tab>Preview</Tab>
+        </TabList>
 
-            <TabList className='appitsy-builder-header-tabs' width='60%'>
-                <Tab>Designer</Tab>
-                <Tab>Schema JSON</Tab>
-                <Tab>Preview</Tab>
-            </TabList>
+        <TabPanel style={tabPanelStyle}>
+          <Designer schema={schema} onSchemaChange={(s) => setSchema(s)} />
+        </TabPanel>
 
-            <TabPanel style={{flexGrow: 1}}>
-                <Designer schema={schema} onSchemaChange={s => setSchema(s)} />
-            </TabPanel>
+        <TabPanel style={tabPanelStyle}>JSON</TabPanel>
 
-            <TabPanel style={{flexGrow: 1}}>
-                JSON
-            </TabPanel>
-
-            <TabPanel style={{flexGrow: 1}}>
-                <Renderer schema={schema} data={data} onDataChange={setData} onSubmit={data => alert(JSON.stringify(data))} />
-            </TabPanel>
-        </AppTabs>
-    );
+        <TabPanel style={tabPanelStyle}>
+          <Renderer
+            schema={schema}
+            data={data}
+            onDataChange={setData}
+            onSubmit={(data) => alert(JSON.stringify(data))}
+          />
+        </TabPanel>
+      </AppTabs>
+    </AppRoot>
+  );
 };
 
 export default App;
