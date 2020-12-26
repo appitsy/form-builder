936b07fd6d40b0cb34c0f23133f6744822155b77
Paras Gera <paras@appitsy.com>
2020-12-26 23:57:43 +0530

    Updates

---
+++ precache-manifest.79741c63694f2dc276ebaec27cd6a784.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f8cb5a4d382c81cbf709e1e8d1b8e34",
+    "url": "/form-builder/index.html"
+  },
+  {
+    "revision": "d64c180e32c289ae0641",
+    "url": "/form-builder/static/css/2.dcf575bd.chunk.css"
+  },
+  {
+    "revision": "63bab3bdb8d776c08230",
+    "url": "/form-builder/static/css/main.174850d8.chunk.css"
+  },
+  {
+    "revision": "d64c180e32c289ae0641",
+    "url": "/form-builder/static/js/2.1cb0eac6.chunk.js"
+  },
+  {
+    "revision": "c836aaadfe43dfd9fcb8ddeddfd61e18",
+    "url": "/form-builder/static/js/2.1cb0eac6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "63bab3bdb8d776c08230",
+    "url": "/form-builder/static/js/main.0bcf3ef3.chunk.js"
+  },
+  {
+    "revision": "2fc96a7f21d72347b2ff",
+    "url": "/form-builder/static/js/runtime-main.3a121d9a.js"
+  }
+]);
